23d798ac120baae755e26b01b434c9b5eea1116c
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-14 12:44:27 +0900

    좌측 메뉴 생성 완료

---
+++ bloom/WebContent/js/side_main.js
@@ -0,0 +1,121 @@
+const scrollmenu = document.querySelectorAll(".left ul li > a");
+ 
+
+// 스크롤 내릴때 이벤트
+
+function handleScrollEvent() {
+  console.log(scrollmenu);
+  console.log(window.scrollX, window.scrollY);
+  if (window.scrollY < 869 || window.scrollY == 0) {
+    scrollmenu[0].classList.add('active');
+    scrollmenu[1].classList.remove('active');
+    scrollmenu[2].classList.remove('active');
+    scrollmenu[3].classList.remove('active');
+  } else if (window.scrollY >= 869 && window.scrollY < 1800) {
+    scrollmenu[0].classList.remove('active');
+    scrollmenu[1].classList.add('active')
+    scrollmenu[2].classList.remove('active')
+    scrollmenu[3].classList.remove('active')
+  } else if (window.scrollY >= 1800 && window.scrollY < 2700) {
+    scrollmenu[0].classList.remove('active');
+    scrollmenu[1].classList.remove('active')
+    scrollmenu[2].classList.add('active')
+    scrollmenu[3].classList.remove('active')
+  } else if (window.scrollY >= 2700 && window.scrollY < 3600) {
+    scrollmenu[0].classList.remove('active');
+    scrollmenu[1].classList.remove('active')
+    scrollmenu[2].classList.remove('active')
+    scrollmenu[3].classList.add('active')
+  }
+}
+
+window.addEventListener('scroll', handleScrollEvent);
+
+// 클릭 했을 때 이벤트
+
+function handleClickEvent(index) {
+  console.log(scrollmenu[index]);
+  scrollmenu.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+
+
+scrollmenu.forEach((item, index) => {
+  item.addEventListener("click", function (e) {
+    handleClickEvent(index);
+  });
+});
+
+function clickToSection(index) {
+  switch (index) {
+    case 0:
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+      break;
+    case 1:
+      window.scrollTo({
+        top: 969,
+        left: 0,
+        behavior: 'smooth'
+      });
+      break;
+    case 2:
+      window.scrollTo({
+        top: 1938,
+        left: 0,
+        behavior: 'smooth'
+      });
+      break;
+    case 3:
+      window.scrollTo({
+        top: 2907,
+        left: 0,
+        behavior: 'smooth'
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+scrollmenu.forEach(function (menu, index) {
+  menu.onclick = function () {
+    clickToSection(index);
+  };
+});
+
+
+// 글자 클릭 이벤트
+const clickmenu = document.querySelectorAll(".left ul li div a");
+
+function ClickEvent(index) {
+  console.log(clickmenu[index]);
+  clickmenu.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
+}
+
+clickmenu.forEach((item, index) => {
+  item.addEventListener("click", function (e) {
+    ClickEvent(index);
+  });
+});
+
+
+clickmenu.forEach(function (menu, index) {
+  menu.onclick = function () {
+    clickToSection(index);
+  };
+});
