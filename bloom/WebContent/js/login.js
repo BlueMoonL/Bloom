ba90d22f985a6a185efb7dacb8a450b8d17ba0ad
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-17 11:40:30 +0900

    다시

--- bloom/WebContent/js/login.js
+++ bloom/WebContent/js/login.js
@@ -10,10 +10,10 @@ document.querySelector("#btn-login").addEventListener("click", open)
 document.querySelector("#login-close").addEventListener("click", close)
 
 var loginBtn = document.getElementById("loginBtn"); //로그인 창안에서 로그인 버튼
-	var userID = document.getElementById("userID");
-	var userPW = document.getElementById("userPW");
+var userID = document.getElementById("userID");
+var userPW = document.getElementById("userPW");
 
-	//로그인 창에서 로그인 버튼을 눌렀을 때
+//로그인 창에서 로그인 버튼을 눌렀을 때
 loginBtn.addEventListener("click", function(e) {
 	fetch("http://localhost:8080/bloom/login", {
 	  method: "POST",
