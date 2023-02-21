c51ac80ff4d4d5508e42360841ba5db30845ba6b
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-21 16:56:27 +0900

    아이디 비밀번호 확인창

--- bloom/WebContent/js/login.js
+++ bloom/WebContent/js/login.js
@@ -5,42 +5,31 @@ function close() {
 	 document.querySelector(".background-login").className = "background-login"
 }
 
-var btnLogin = document.querySelector("#btn-login");
-var loginClose = document.querySelector("#login-close");
-
-if (btnLogin != null) {
-	btnLogin.addEventListener("click", open);
-}
-
-if (loginClose != null) {
-	document.querySelector("#login-close").addEventListener("click", close);
-}
+document.querySelector("#btn-login").addEventListener("click", open)
+document.querySelector("#login-close").addEventListener("click", close)
 
 var loginBtn = document.getElementById("loginBtn"); //로그인 창안에서 로그인 버튼
 var userID = document.getElementById("userID");
 var userPW = document.getElementById("userPW");
-
 //로그인 창에서 로그인 버튼을 눌렀을 때
-
-if (loginBtn != null) {
-	loginBtn.addEventListener("click", function(e) {
-		fetch("http://localhost:8080/bloom/login", {
-		  method: "POST",
-		  headers: {
-			    "Content-Type": "application/json",
-			  },
-		  body: JSON.stringify({
-		           "userID" : userID.value,
-		           "userPW" : userPW.value
-			})
+loginBtn.addEventListener("click", function(e) {
+	fetch("http://localhost:8080/bloom/login", {
+	  method: "POST",
+	  headers: {
+		    "Content-Type": "application/json",
+		  },
+	  body: JSON.stringify({
+	           "userID" : userID.value,
+	           "userPW" : userPW.value
 		})
-		.then(response => response.text())
-		.then((result) => {
-			console.log(result);
-				if (result != "") {
-					close();
-					location.reload();
-				}
-			})
+	})
+	.then(response => response.json())
+	.then((json) => {
+			if (json.result == 1) {
+				close();
+				location.reload();
+			} else {
+				window.alert("아이디, 비밀번호를 확인하세요");
+			}
 		})
-}
+	})
