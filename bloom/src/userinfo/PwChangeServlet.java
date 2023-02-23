dd99de418d1b2437b4167ba31352c5b2306cce0a
박가영 <GGG@DESKTOP-8N3GG4O>
2023-02-23 16:48:42 +0900

    다시 올라가

--- bloom/src/userinfo/PwChangeServlet.java
+++ bloom/src/userinfo/PwChangeServlet.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @WebServlet("/pwChange")
 public class PwChangeServlet extends HttpServlet {
 
-	@Override
+	@Override  
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
