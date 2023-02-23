d5fe9f015d31179388de1eabb674d785d88726fa
GGG <GGG@DESKTOP-8N3GG4O>
2023-02-23 15:16:45 +0900

    왜안떠

--- bloom/src/Doctor/DTImage.java
+++ bloom/src/Doctor/DTImage.java
@@ -1,10 +1,8 @@
 package Doctor;
 
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Blob;
 import java.sql.Connection;
@@ -12,7 +10,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.StringTokenizer;
 
 import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +23,7 @@ public class DTImage extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		// 세부경로를 찾아주는 메서드
-		
+
 		String uri = req.getRequestURI();
 		/// bloom/dtimage/1 뒤의 숫자를 기준으로 새로운 주소와 이미지를 설정하면 되나 ?
 		String[] split = uri.split("/");
@@ -47,18 +44,24 @@ public class DTImage extends HttpServlet {
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, Integer.parseInt(dbName));
 			ResultSet rs = stmt.executeQuery();
+
 			byte[] imgBytes = null;
 
 			if (rs.next()) {
-				Blob blob = rs.getBlob("image");
-				imgBytes = blob.getBytes(1, (int) blob.length());
+//				Blob blob = rs.getBlob("image");
+				imgBytes = rs.getBytes("image");
+//				imgBytes = 
+				ByteArrayInputStream bais = new ByteArrayInputStream(imgBytes);
+				BufferedImage image = ImageIO.read(bais);
 
-				InputStream in = new ByteArrayInputStream(imgBytes);
-				Image img = ImageIO.read(in);
+//						blob.getBytes(1, (int) blob.length());
+//
+//				InputStream in = new ByteArrayInputStream(imgBytes);
+//				Image img = ImageIO.read(in);
 
-				resp.setContentType("image/png");
+//				resp.setContentType("image/png");
 				OutputStream out = resp.getOutputStream();
-				ImageIO.write((BufferedImage) img, "png", out);
+				ImageIO.write((BufferedImage) image, "png", out);
 				out.close();
 			}
 
